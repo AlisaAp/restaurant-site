c5ef52606349a8f37e90a6af9b3fdf973b371d18
Alisa Apotovska <masanaapot@gmail.com>
2023-11-17 14:14:58 +0200

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
   return (
-    <div />
+    <Box sx={{
+      textAlign: 'center', bgcolor: 'black', color: 'white', p: 1,
+    }}
+    >
+      <Box sx={{
+        my: 1,
+        '& svg': { fontSize: '2rem', cursor: 'pointer', mr: 2 },
+        '& svg:hover': { color: '#ced970', transform: 'translateX(3px)', transition: 'all 0.4s' },
+      }}
+      >
+        <InstagramIcon />
+        <FacebookIcon />
+        <TwitterIcon />
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{
+          '@media (max-width:600px)': {
+            fontSize: '1rem',
+          },
+        }}
+      >
+        All Rights Reserved &copy;
+      </Typography>
+    </Box>
   );
 }
 
